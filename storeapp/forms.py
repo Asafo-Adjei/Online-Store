b17b98b00ac76dd33ecca99289509697081deabf
Papa Yaw Asafo-Adjei <yawadjei2003@gmail.com>
2025-07-23 11:37:08 +0000

    Add gunicorn and Procfile

--- storeapp/forms.py
+++ storeapp/forms.py
@@ -1,6 +1,6 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
+from django import forms # type: ignore
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm # type: ignore
+from django.contrib.auth.models import User # type: ignore
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget = forms.TextInput(attrs= {
